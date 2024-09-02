b3ef9ccf5f9e06742a221b5bfb3ed41fdceccfb1
shoyonp <shoyonkumar143@gmail.com>
2024-09-03 00:39:41 +0600

    object done

---
+++ object.js
@@ -0,0 +1,32 @@
+// primitive
+// basic, primary datatype
+const age = 21;
+const school = 'ok bye'
+const isPassed = true;
+// let isDeveloper;
+
+const subjects = ['bangla', 'english', 'physics', 'math']
+// const bottle = ['white', 45]
+
+
+
+// object
+// non primitive
+const bottle = {
+    brand: 'mum',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+// non primitive
+const subject = {
+    name: 'biology',
+    teacher: 'shuvro vai',
+    examDate: '28 july',
+    chapters: ['first', 'second', 'thrid'],
+    exams: {
+        name:  'final',
+        marks: 100
+    }
+}
